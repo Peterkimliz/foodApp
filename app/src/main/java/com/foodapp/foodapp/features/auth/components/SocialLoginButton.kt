27c85358a484b06bd004d28591640dda610ed29d
PeterKimLiz <pkironji@yahoo.com>
2025-07-02 10:12:41 +0300

    refactored the code

---
+++ app/src/main/java/com/foodapp/foodapp/features/auth/components/SocialLoginButton.kt
@@ -0,0 +1,53 @@
+package com.foodapp.foodapp.features.auth.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SocialLoginButton(
+    @DrawableRes image: Int,
+    title: Int,
+    callback: () -> Unit
+
+) {
+    Button(
+
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+
+            ),
+        onClick = callback,
+        elevation = ButtonDefaults.elevatedButtonElevation(
+            defaultElevation = 15.dp
+        )
+    ) {
+
+        Row(
+            modifier = Modifier.padding(horizontal = 5.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = null
+            )
+            Spacer(Modifier.width(5.dp))
+            Text(text = stringResource(title))
+        }
+    }
+}
